29e9d32b97cfc2307b8ed843335c6e16a4f7d901
TomoyaKamijima <tomoshangdao99@gmail.com>
2023-06-02 16:26:04 +0900

    blog8-2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,5 +13,10 @@ class Post extends Model
     {
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
 
