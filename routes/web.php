d77e6c49816371a58f672b151d700e65d2328e1c
TomoyaKamijima <tomoshangdao99@gmail.com>
2023-06-08 00:00:31 +0900

    ブログ投稿作成機能の追加

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+
+Route::post('/posts', [PostController::class, 'store']);
